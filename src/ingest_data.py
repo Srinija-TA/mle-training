7fc17f00aeaff2a69b915ef52529fec3bfbc10d7
Srinija-TA <srinija.tummepal@tigeranalytics.com>
2023-12-10 23:59:03 +0530

    Revert "mlflow ui"
    This reverts commit fb46ff27f90876732bc7effbcea70c2f3a568f53.

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,20 +1,44 @@
-import logging
 import os
 import tarfile
+import urllib
 
 import numpy as np
 import pandas as pd
-from six.moves import urllib  # pyright: ignore
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
-HOUSING_PATH = os.path.join("../data", "raw")
+HOUSING_PATH = "/home/srinija2001/mle-training"
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
-logger = logging.getLogger(__name__)
+
+def get_data(output_folder):
+    """Fetches housing data from a given URL and return it.
+
+    Parameters
+    -----------
+    None
+
+    Returns
+    --------
+    housing data
+
+    """
+    housing = load_housing_data()
+
+    # train_set, test_set = train_test_split(
+    # housing, test_size=0.2,
+    # random_state=42
+    # )
+
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+    return housing
 
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-    """Fetches housing data from the given URL and extracts it to a specified path.
+    """Fetches housing data from a given URL and extracts it to path.
 
     Parameters
     -----------
@@ -28,7 +52,9 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     None
 
     """
-    logger.info("Fetches housing data from URL and extracting into specified path")
+
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
@@ -38,45 +64,20 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 
 def load_housing_data(housing_path=HOUSING_PATH):
-    """Loads housing data from a CSV file.
+    """Loads housing data from CSV file.
 
     Parameters
     -----------
+
     housing_path : str
     Path to the housing data CSV file.
 
     Returns
     --------
+
     pd.DataFrame:Pandas DataFrame containing the loaded housing data.
 
     """
-    logger.info("Loads housing data from a CSV file")
+
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
-
-
-def get_data(output_folder):
-    """Fetches housing data from a given URL and return it.
-
-    Parameters
-    -----------
-    output_folder : str
-    folder path to store the dataset
-
-    Returns
-    --------
-    pd.DataFrame:Pandas DataFrame containing the loaded housing data.
-
-    """
-    logger.info("Fetches housing data from a given URL and returning it")
-    fetch_housing_data()
-    housing = load_housing_data()
-    housing
-    housing["income_cat"] = pd.cut(
-        housing["median_income"],
-        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
-        labels=[1, 2, 3, 4, 5],
-    )
-    output_dataset_path = os.path.join(output_folder, "housing.csv")
-    housing.to_csv(output_dataset_path, index=False)
-    return housing
